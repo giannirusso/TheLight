634679d8d623399122afeaf8a9bc17b251986416
Djanibek Khudaybergenov <djanibek.k@gmail.com>
2020-12-10 16:15:08 +0300

    Added AVFounation and realTorch element

--- The Light/ViewController.swift
+++ The Light/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -15,7 +16,6 @@ class ViewController: UIViewController {
         return true
     }
     
-    // MARK: - Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +25,27 @@ class ViewController: UIViewController {
     fileprivate func updateUI() {
         
         view.backgroundColor = isLightOn ? .white : .black
+        
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        guard device.hasTorch else { return }
+        
+        do {
+            try device.lockForConfiguration()
+            
+            if (device.torchMode == AVCaptureDevice.TorchMode.on) {
+                device.torchMode = AVCaptureDevice.TorchMode.off
+            } else {
+                do {
+                    try device.setTorchModeOn(level: 1.0)
+                } catch {
+                    print(error)
+                }
+            }
+            
+            device.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
